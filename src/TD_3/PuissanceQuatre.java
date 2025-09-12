f4366c982e40eff067a9da908558e1ee0f67f8f5
antoine blanquaert <ablanquaert@pouchain.fr>
2025-09-12 11:25:24 +0200

    refactor(PuissanceQuatre): better display + regroup in functions

--- src/TD_3/PuissanceQuatre.java
+++ src/TD_3/PuissanceQuatre.java
@@ -3,45 +3,53 @@ package TD_3;
 import java.util.Scanner;
 
 public class PuissanceQuatre {
-
+	static Scanner scanner = new Scanner(System.in);
     static final int VIDE = 0;
     static final int ROUGE = 1;
     static final int JAUNE = 2;
 
     public static int[][] nouvelleGrille(int largeur, int hauteur) {
         int[][] grille = new int[largeur][hauteur];
-        for (int i = 0; i < largeur; i++) {
-            for (int j = 0; j < hauteur; j++) {
+        for (int i = 0; i <largeur; i++) {
+            for (int j = 0; j <hauteur; j++) {
                 grille[i][j] = VIDE;
             }
         }
         return grille;
     }
+    
+    public static void displayCellValue(int cellValue) {
+        switch (cellValue) {
+        case VIDE:
+            System.out.print("| |");
+            break;
+        case ROUGE:
+            System.out.print("|R|");
+            break;
+        case JAUNE:
+            System.out.print("|J|");
+            break;
+    }
+    }
 
     public static void afficheGrille(int[][] grille) {
-        System.out.println("________________________");
         for (int i = 0; i < grille.length; i++) {
             for (int j = 0; j < grille[i].length; j++) {
                 int cellValue = grille[i][j];
-                switch (cellValue) {
-                    case VIDE:
-                        System.out.print("| |");
-                        break;
-                    case ROUGE:
-                        System.out.print("|R|");
-                        break;
-                    case JAUNE:
-                        System.out.print("|J|");
-                        break;
-                }
+                displayCellValue(cellValue);
 
             }
             System.out.println();
         }
+        System.out.println("\n");
+        for(int i = 1;i<=grille[0].length;i++) {
+        	System.out.print("|"+i+"|");
+        }
+        System.out.println();
+        
     }
 
     public static boolean grilleGagnante(int[][] grille) {
-        // Check for a win for both players (assuming player 1 and 2)
         for (int joueur = 1; joueur <= 2; joueur++) {
             // Check for horizontal wins
             for (int i = 0; i < grille.length; i++) {
@@ -100,7 +108,6 @@ public class PuissanceQuatre {
                     }
                 }
                 if (cptStreak == 4) {
-                    System.out.println("a gagné");
                     return true;
                 }
             }
@@ -119,7 +126,6 @@ public class PuissanceQuatre {
             }
 
             if (cptStreak == 4) {
-                System.out.println("a gagné");
                 return true;
             }
         }
@@ -135,51 +141,52 @@ public class PuissanceQuatre {
 
 
         for (int i = grille.length - 1; i >= 0; i--) {
+        	
             if (grille[i][colonne] == 0) {
+            	// normal play
+            	grille[i][colonne] = joueur;
                 grille[i][colonne] = joueur;
-
-            grille[i][colonne] = joueur;
-            afficheGrille(grille);
-
+                afficheGrille(grille);
+            	
+                
+            	colonne++;
                 System.out.println("Jeton du joueur " + joueur + " placé en colonne " + colonne + ".");
                 return true;
             }
         }
 
-
+        colonne++;
         System.out.println("Erreur : La colonne " + colonne + " est pleine.");
-        return false; // Token can't be played
+        return false; // can't place it
     }
 
     public static int demanderColonne(Scanner scanner){
-//        Scanner sc  = new Scanner(System.in);
         System.out.println("jouer dans la col n° ?");
         int numColonne = scanner.nextInt();
-//        sc.close();
         return numColonne-1;
     }
 
     public static boolean tourJoueur(int [] [] grille, String nomJoueur, int couleur,Scanner scanner)  {
         boolean hasWin = false;
+        
         int numCol = demanderColonne(scanner);
+        
+        // launch play sequence
         if(!joueJeton(grille, numCol, couleur)){
             numCol =   demanderColonne(scanner);
         }
+        
+        // check hasWin
         hasWin = grilleGagnante(grille);
         return hasWin;
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[][] test = nouvelleGrille(7, 6);
-        afficheGrille(test);
-
-
-
+    
+    static void CycleParty(int[][]grille) {
         boolean hasWin = false;
         int tourJoueur = ROUGE;
-        do{
-            hasWin = tourJoueur(test,"antoine",tourJoueur,scanner);
+        
+        do {
+            hasWin = tourJoueur(grille,"antoine",tourJoueur,scanner);
             if(!hasWin){
                 switch(tourJoueur){
                     case ROUGE:
@@ -191,6 +198,16 @@ public class PuissanceQuatre {
                 }
             }
         } while(!hasWin);
+        
+        System.out.println("Le joueur " + tourJoueur + " a gagné !");
+    }
+    
+    public static void main(String[] args) {
+        int[][] grille = nouvelleGrille(7, 6);
+        
+        afficheGrille(grille);
+        CycleParty(grille);
+        
         scanner.close();
     }
 }
